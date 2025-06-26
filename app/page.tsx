09672f27dd889ed7158a34910d0c63e2fc387fc0
nathan-ferchtandiker <nferchtandiker@gmail.com>
2025-06-25 20:34:01 -0700

    stash

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,8 @@ export default function Home() {
 
   const handleSendMessage = async (message: string) => {
     try {
-      const res = await fetch('http://localhost:5000/api/app', {
+      const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+      const res = await fetch(`${apiUrl}/api/app`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ message }),
